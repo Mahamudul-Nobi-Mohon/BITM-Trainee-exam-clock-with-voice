4697647cbdb8b24cc53032eb9d0d34dc406265ef
Mahamudul-Nobi-Mohon <mn.mohon@gmail.com>
2016-10-19 22:14:30 +0600

    new project add

---
+++ ExamTimeShowForBITM/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ExamTimpeShowForBITM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.startTimeButton = new System.Windows.Forms.Button();
+            this.startTimeTextBox = new System.Windows.Forms.TextBox();
+            this.remainingTimeLabel = new System.Windows.Forms.Label();
+            this.minuteLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(188, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "BITM TRAINEE EXAM CLOCK";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(680, 298);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(114, 30);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(539, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.startTimeButton);
+            this.groupBox1.Controls.Add(this.startTimeTextBox);
+            this.groupBox1.Controls.Add(this.minuteLabel);
+            this.groupBox1.Location = new System.Drawing.Point(7, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 190);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Exam Time";
+            // 
+            // startTimeButton
+            // 
+            this.startTimeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startTimeButton.Location = new System.Drawing.Point(69, 119);
+            this.startTimeButton.Name = "startTimeButton";
+            this.startTimeButton.Size = new System.Drawing.Size(158, 41);
+            this.startTimeButton.TabIndex = 9;
+            this.startTimeButton.Text = "Start Time";
+            this.startTimeButton.UseVisualStyleBackColor = true;
+            this.startTimeButton.Click += new System.EventHandler(this.startTimeButton_Click);
+            // 
+            // startTimeTextBox
+            // 
+            this.startTimeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startTimeTextBox.Location = new System.Drawing.Point(46, 39);
+            this.startTimeTextBox.Name = "startTimeTextBox";
+            this.startTimeTextBox.Size = new System.Drawing.Size(100, 31);
+            this.startTimeTextBox.TabIndex = 8;
+            // 
+            // remainingTimeLabel
+            // 
+            this.remainingTimeLabel.AutoSize = true;
+            this.remainingTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.remainingTimeLabel.Location = new System.Drawing.Point(21, 63);
+            this.remainingTimeLabel.Name = "remainingTimeLabel";
+            this.remainingTimeLabel.Size = new System.Drawing.Size(172, 42);
+            this.remainingTimeLabel.TabIndex = 3;
+            this.remainingTimeLabel.Text = "00:00:00";
+            this.remainingTimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minuteLabel
+            // 
+            this.minuteLabel.AutoSize = true;
+            this.minuteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteLabel.Location = new System.Drawing.Point(152, 46);
+            this.minuteLabel.Name = "minuteLabel";
+            this.minuteLabel.Size = new System.Drawing.Size(75, 20);
+            this.minuteLabel.TabIndex = 5;
+            this.minuteLabel.Text = "(Minute)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.remainingTimeLabel);
+            this.groupBox2.Location = new System.Drawing.Point(437, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(334, 197);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Remaining Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 337);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label5);
+            this.Name = "Form1";
+            this.Text = "BITM Trainee Exam Clock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button startTimeButton;
+        private System.Windows.Forms.TextBox startTimeTextBox;
+        private System.Windows.Forms.Label remainingTimeLabel;
+        private System.Windows.Forms.Label minuteLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ ExamTimeShowForBITM/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Media;
+
+namespace ExamTimpeShowForBITM
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer t;
+        private int h;
+        private int m;
+        private int s = 60;
+
+        private SoundPlayer soundplayer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void startTimeButton_Click(object sender, EventArgs e)
+        {
+            soundplayer = new SoundPlayer("startExam.wav");
+            soundplayer.Play();
+            try
+            {
+                remainingTimeLabel.Text = "00:00:00" + "(h:m:s)";
+            
+                int input = Convert.ToInt32(startTimeTextBox.Text);
+           
+                m = input - 1;
+
+                if (remainingTimeLabel.Text == "00:00:00(h:m:s)")
+                {
+
+                    t.Start();
+                    startTimeButton.Enabled = false;
+                    
+                }
+
+
+            
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Please set a start time.");
+            }
+           
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new System.Timers.Timer();
+            t.Interval = 1000;
+            t.Elapsed += OnTimeEvent;
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            if (m > -1)
+            {
+                Invoke(new Action(() =>
+                {
+                    s -= 1;
+                    if (s == 0)
+                    {
+                        s = 60;
+                        m -= 1;
+
+                    }
+                   
+                    if (m == -1 || m < -1)
+                    {
+                        remainingTimeLabel.Text = "00:00:00"+"(h:m:s)";
+                        remainingTimeLabel.ForeColor = Color.Red;
+                       
+                    }
+                    else
+                    {
+                        remainingTimeLabel.Text = string.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'),
+                            m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0')) + "(h:m:s)";
+                        remainingTimeLabel.ForeColor = Color.Green;
+                    }
+                }));
+            }
+
+            if (m == -1 || m < -1)
+            {
+                s = 0;
+                t.Stop();
+                soundplayer = new SoundPlayer("stopWriting.wav");
+                soundplayer.Play();
+                MessageBox.Show("Please Stop Writing. ", "Time Elapsed");
+               
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            t.Stop();
+            Application.DoEvents();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            s = 60;
+            m = 0;
+            h = 0;
+            remainingTimeLabel.Text = "00:00:00"+"(h:m:s)";
+            startTimeButton.Enabled = true;
+            t.Stop();
+            Application.DoEvents();
+            
+        }
+
+        
+
+    }
+
+
+}
